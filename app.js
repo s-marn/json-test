4b45c3d406eb644d7d532a5c54b706014779f898
marn <150642867+s-marn@users.noreply.github.com>
2024-02-27 18:35:48 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//javascript object
+let myObj={
+    "name":"มาน",
+    "age": 41,
+    "phone":null,
+    "status":true,
+    "languages" : ["ไทย","อังกฤษ","จีน"],
+    "general" : {
+        "weight":70,
+        "height":170,
+        "gender":"ชาย"
+    }
+}
+console.log(myObj.languages[0])
+console.log(myObj.general.gender)
+
